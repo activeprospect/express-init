faa968273f797271af34fd427ec63271509b7859
Blake Sager <blake.sager@activeprospect.com>
2020-02-20 13:01:23 -0600

    cleanup '

--- test/init-spec.js
+++ test/init-spec.js
@@ -1,117 +1,105 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const {
-  assert
-} = require('chai');
+
+const { assert } = require('chai');
 const express = require('express');
 const init = require('../lib');
 
-describe('Init', function() {
-
+describe('Init', function () {
   let middleware1 = null;
   let middleware2 = null;
   let app = null;
 
-  beforeEach(function() {
+  beforeEach(function () {
     app = express();
     app.initialized = [];
 
-    middleware1 = function(req, res, next) {
+    middleware1 = function (req, res, next) {
       req.one = true;
-      return next();
+      next();
     };
-    middleware1.init = function(app, callback) {
+    middleware1.init = function (app, callback) {
       app.initialized.push(middleware1);
-      return callback();
+      callback();
     };
 
-    middleware2 = function(req, res, next) {
+    middleware2 = function (req, res, next) {
       req.two = true;
-      return next();
+      next();
     };
-    return middleware2.init = function(app, callback) {
+    middleware2.init = function (app, callback) {
       app.initialized.push(middleware2);
-      return callback();
+      callback();
     };
   });
 
-  it('should process middlewares in order', function(done) {
+  it('should process middlewares in order', function (done) {
     app.use(middleware1);
     app.use(middleware2);
-    return init(app, function(err) {
+    init(app, function (err) {
       if (err) { return done(err); }
       assert.deepEqual(app.initialized, [middleware1, middleware2]);
-      return done();
+      done();
     });
   });
 
-  it('should process middlewares of routes', function(done) {
+  it('should process middlewares of routes', function (done) {
     app.use(middleware1);
     app.route('/')
       .all(middleware2);
 
-    return init(app, function(err) {
+    init(app, function (err) {
       if (err) { return done(err); }
       assert.deepEqual(app.initialized, [middleware1, middleware2]);
-      return done();
+      done();
     });
   });
 
-
-  it('should process middlewares of routers', function(done) {
+  it('should process middlewares of routers', function (done) {
     const router = new express.Router();
     router.use(middleware1);
     router.use(middleware2);
     app.use(router);
 
-    return init(app, function(err) {
+    init(app, function (err) {
       if (err) { return done(err); }
       assert.deepEqual(app.initialized, [middleware1, middleware2]);
-      return done();
+      done();
     });
   });
 
-
-  it('should process routers', function(done) {
+  it('should process routers', function (done) {
     const router = new express.Router();
-    router.init = function(app, callback) {
+    router.init = function (app, callback) {
       app.initialized.push(router);
-      return callback();
+      callback();
     };
     app.use(router);
 
-    return init(app, function(err) {
+    init(app, function (err) {
       if (err) { return done(err); }
       assert.deepEqual(app.initialized, [router]);
-      return done();
+      done();
     });
   });
 
-
-  it('should process nested routers', function(done) {
+  it('should process nested routers', function (done) {
     const nestedRouter = new express.Router();
-    nestedRouter.init = function(app, callback) {
+    nestedRouter.init = function (app, callback) {
       app.initialized.push(nestedRouter);
-      return callback();
+      callback();
     };
 
     const router = new express.Router();
     router.use(nestedRouter);
     app.use(router);
 
-    return init(app, function(err) {
+    init(app, function (err) {
       if (err) { return done(err); }
       assert.deepEqual(app.initialized, [nestedRouter]);
-      return done();
+      done();
     });
   });
 
-
-  it('should process middlewares of nested routers', function(done) {
+  it('should process middlewares of nested routers', function (done) {
     const nestedRouter = new express.Router();
     nestedRouter.use(middleware2);
 
@@ -120,53 +108,47 @@ describe('Init', function() {
     router.use(nestedRouter);
     app.use(router);
 
-    return init(app, function(err) {
+    init(app, function (err) {
       if (err) { return done(err); }
       assert.deepEqual(app.initialized, [middleware1, middleware2]);
-      return done();
+      done();
     });
   });
 
-
-  it('should not initialize middleware more than once for the same app', function(done) {
+  it('should not initialize middleware more than once for the same app', function (done) {
     const middleware = (req, res, next) => next();
-    middleware.init = function(app, callback) {
+    middleware.init = function (app, callback) {
       if (middleware.initialized) {
         return callback(new Error('Should not init more than once'));
       }
       app.initialized.push(middleware);
-      return callback();
+      callback();
     };
     app.use(middleware);
     app.use(middleware);
-    return init(app, function(err) {
+    init(app, function (err) {
       if (err) { return done(err); }
       assert.deepEqual(app.initialized, [middleware]);
-      return done();
+      done();
     });
   });
 
-
-  return it('should initialize middleware for each app', function(done) {
+  it('should initialize middleware for each app', function (done) {
     app.use(middleware1);
 
     const app2 = express();
-    if (app2.initialized == null) { app2.initialized = []; }
+    if (!app2.initialized) { app2.initialized = []; }
     app2.use(middleware1);
 
-    return init(app, function(err) {
+    init(app, function (err) {
       if (err) { return done(err); }
       assert.deepEqual(app.initialized, [middleware1]);
 
-      return init(app2, function(err) {
+      init(app2, function (err) {
         if (err) { return done(err); }
         assert.deepEqual(app2.initialized, [middleware1]);
-        return done();
+        done();
       });
     });
   });
 });
-
-
-
-
