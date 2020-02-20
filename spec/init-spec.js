b871189def0f85a6c9f588df415808cd6178f21e
decaffeinate <blake.sager@activeprospect.com>
2020-02-20 12:51:42 -0600

    decaffeinate: Convert init-spec.coffee to JS

--- spec/init-spec.js
+++ spec/init-spec.js
@@ -1,137 +1,171 @@
-assert = require('chai').assert
-express = require('express')
-init = require('../lib')
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const {
+  assert
+} = require('chai');
+const express = require('express');
+const init = require('../lib');
 
-describe 'Init', ->
+describe('Init', function() {
 
-  middleware1 = null
-  middleware2 = null
-  app = null
+  let middleware1 = null;
+  let middleware2 = null;
+  let app = null;
 
-  beforeEach ->
-    app = express()
-    app.initialized = []
+  beforeEach(function() {
+    app = express();
+    app.initialized = [];
 
-    middleware1 = (req, res, next) ->
-      req.one = true
-      next()
-    middleware1.init = (app, callback) ->
-      app.initialized.push(middleware1)
-      callback()
+    middleware1 = function(req, res, next) {
+      req.one = true;
+      return next();
+    };
+    middleware1.init = function(app, callback) {
+      app.initialized.push(middleware1);
+      return callback();
+    };
 
-    middleware2 = (req, res, next) ->
-      req.two = true
-      next()
-    middleware2.init = (app, callback) ->
-      app.initialized.push(middleware2)
-      callback()
+    middleware2 = function(req, res, next) {
+      req.two = true;
+      return next();
+    };
+    return middleware2.init = function(app, callback) {
+      app.initialized.push(middleware2);
+      return callback();
+    };
+  });
 
-  it 'should process middlewares in order', (done) ->
-    app.use middleware1
-    app.use middleware2
-    init app, (err) ->
-      return done(err) if err
-      assert.deepEqual app.initialized, [middleware1, middleware2]
-      done()
+  it('should process middlewares in order', function(done) {
+    app.use(middleware1);
+    app.use(middleware2);
+    return init(app, function(err) {
+      if (err) { return done(err); }
+      assert.deepEqual(app.initialized, [middleware1, middleware2]);
+      return done();
+    });
+  });
 
-  it 'should process middlewares of routes', (done) ->
-    app.use middleware1
+  it('should process middlewares of routes', function(done) {
+    app.use(middleware1);
     app.route('/')
-      .all middleware2
+      .all(middleware2);
 
-    init app, (err) ->
-      return done(err) if err
-      assert.deepEqual app.initialized, [middleware1, middleware2]
-      done()
-
-
-  it 'should process middlewares of routers', (done) ->
-    router = new express.Router()
-    router.use middleware1
-    router.use middleware2
-    app.use router
-
-    init app, (err) ->
-      return done(err) if err
-      assert.deepEqual app.initialized, [middleware1, middleware2]
-      done()
+    return init(app, function(err) {
+      if (err) { return done(err); }
+      assert.deepEqual(app.initialized, [middleware1, middleware2]);
+      return done();
+    });
+  });
 
 
-  it 'should process routers', (done) ->
-    router = new express.Router()
-    router.init = (app, callback) ->
-      app.initialized.push(router)
-      callback()
-    app.use router
+  it('should process middlewares of routers', function(done) {
+    const router = new express.Router();
+    router.use(middleware1);
+    router.use(middleware2);
+    app.use(router);
 
-    init app, (err) ->
-      return done(err) if err
-      assert.deepEqual app.initialized, [router]
-      done()
-
-
-  it 'should process nested routers', (done) ->
-    nestedRouter = new express.Router()
-    nestedRouter.init = (app, callback) ->
-      app.initialized.push(nestedRouter)
-      callback()
-
-    router = new express.Router()
-    router.use nestedRouter
-    app.use router
-
-    init app, (err) ->
-      return done(err) if err
-      assert.deepEqual app.initialized, [nestedRouter]
-      done()
+    return init(app, function(err) {
+      if (err) { return done(err); }
+      assert.deepEqual(app.initialized, [middleware1, middleware2]);
+      return done();
+    });
+  });
 
 
-  it 'should process middlewares of nested routers', (done) ->
-    nestedRouter = new express.Router()
-    nestedRouter.use middleware2
+  it('should process routers', function(done) {
+    const router = new express.Router();
+    router.init = function(app, callback) {
+      app.initialized.push(router);
+      return callback();
+    };
+    app.use(router);
 
-    router = new express.Router()
-    router.use middleware1
-    router.use nestedRouter
-    app.use router
-
-    init app, (err) ->
-      return done(err) if err
-      assert.deepEqual app.initialized, [middleware1, middleware2]
-      done()
-
-
-  it 'should not initialize middleware more than once for the same app', (done) ->
-    middleware = (req, res, next) ->
-      next()
-    middleware.init = (app, callback) ->
-      if middleware.initialized
-        return callback(new Error('Should not init more than once'))
-      app.initialized.push(middleware)
-      callback()
-    app.use middleware
-    app.use middleware
-    init app, (err) ->
-      return done(err) if err
-      assert.deepEqual app.initialized, [middleware]
-      done()
+    return init(app, function(err) {
+      if (err) { return done(err); }
+      assert.deepEqual(app.initialized, [router]);
+      return done();
+    });
+  });
 
 
-  it 'should initialize middleware for each app', (done) ->
-    app.use middleware1
+  it('should process nested routers', function(done) {
+    const nestedRouter = new express.Router();
+    nestedRouter.init = function(app, callback) {
+      app.initialized.push(nestedRouter);
+      return callback();
+    };
 
-    app2 = express()
-    app2.initialized ?= []
-    app2.use middleware1
+    const router = new express.Router();
+    router.use(nestedRouter);
+    app.use(router);
 
-    init app, (err) ->
-      return done(err) if err
-      assert.deepEqual app.initialized, [middleware1]
+    return init(app, function(err) {
+      if (err) { return done(err); }
+      assert.deepEqual(app.initialized, [nestedRouter]);
+      return done();
+    });
+  });
 
-      init app2, (err) ->
-        return done(err) if err
-        assert.deepEqual app2.initialized, [middleware1]
-        done()
+
+  it('should process middlewares of nested routers', function(done) {
+    const nestedRouter = new express.Router();
+    nestedRouter.use(middleware2);
+
+    const router = new express.Router();
+    router.use(middleware1);
+    router.use(nestedRouter);
+    app.use(router);
+
+    return init(app, function(err) {
+      if (err) { return done(err); }
+      assert.deepEqual(app.initialized, [middleware1, middleware2]);
+      return done();
+    });
+  });
+
+
+  it('should not initialize middleware more than once for the same app', function(done) {
+    const middleware = (req, res, next) => next();
+    middleware.init = function(app, callback) {
+      if (middleware.initialized) {
+        return callback(new Error('Should not init more than once'));
+      }
+      app.initialized.push(middleware);
+      return callback();
+    };
+    app.use(middleware);
+    app.use(middleware);
+    return init(app, function(err) {
+      if (err) { return done(err); }
+      assert.deepEqual(app.initialized, [middleware]);
+      return done();
+    });
+  });
+
+
+  return it('should initialize middleware for each app', function(done) {
+    app.use(middleware1);
+
+    const app2 = express();
+    if (app2.initialized == null) { app2.initialized = []; }
+    app2.use(middleware1);
+
+    return init(app, function(err) {
+      if (err) { return done(err); }
+      assert.deepEqual(app.initialized, [middleware1]);
+
+      return init(app2, function(err) {
+        if (err) { return done(err); }
+        assert.deepEqual(app2.initialized, [middleware1]);
+        return done();
+      });
+    });
+  });
+});
 
 
 
