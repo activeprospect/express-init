c042e61de825f235f381073427b43c935073771a
decaffeinate <blake.sager@activeprospect.com>
2020-02-20 12:42:48 -0600

    decaffeinate: Convert index.coffee to JS

--- src/index.js
+++ src/index.js
@@ -1,37 +1,50 @@
-_ = require('lodash')
-async = require('async')
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const _ = require('lodash');
+const async = require('async');
 
 
-recurseStack = (app, stack) ->
-  stack = [stack] unless _(stack).isArray()
+var recurseStack = function(app, stack) {
+  if (!_(stack).isArray()) { stack = [stack]; }
 
-  s = stack.map (layer) ->
+  const s = stack.map(function(layer) {
 
-    initializers = []
+    let initializers = [];
 
-    if layer.route?.stack?
-      initializers = initializers.concat(recurseStack(app, layer.route.stack))
+    if ((layer.route != null ? layer.route.stack : undefined) != null) {
+      initializers = initializers.concat(recurseStack(app, layer.route.stack));
+    }
 
-    if layer.handle?.stack?
-      initializers = initializers.concat(recurseStack(app, layer.handle.stack))
+    if ((layer.handle != null ? layer.handle.stack : undefined) != null) {
+      initializers = initializers.concat(recurseStack(app, layer.handle.stack));
+    }
 
-    init = layer.init or layer.handle?.init
-    if init
-      initializers.push (callback) ->
-        return callback() if app[init] == true
-        args = []
-        args.push app if init.length == 2
-        args.push (err) ->
-          app[init] = !err
-          callback(err)
-        init.apply(init, args)
+    const init = layer.init || (layer.handle != null ? layer.handle.init : undefined);
+    if (init) {
+      initializers.push(function(callback) {
+        if (app[init] === true) { return callback(); }
+        const args = [];
+        if (init.length === 2) { args.push(app); }
+        args.push(function(err) {
+          app[init] = !err;
+          return callback(err);
+        });
+        return init.apply(init, args);
+      });
+    }
 
-    initializers
+    return initializers;
+  });
 
-  _(s).flatten().compact().valueOf()
+  return _(s).flatten().compact().valueOf();
+};
 
 
-module.exports = (app, callback) ->
-  initializers = recurseStack(app, app._router.stack)
-  async.series initializers, (err) ->
-    callback(err)
+module.exports = function(app, callback) {
+  const initializers = recurseStack(app, app._router.stack);
+  return async.series(initializers, err => callback(err));
+};
